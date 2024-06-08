81cee2cfd36ad9e90d77762fa4217d684ae62370
Elufere Innocent <eluferedonatus@gmail.com>
2024-06-08 10:41:09 +0100

    commit

--- backend/index.js
+++ backend/index.js
@@ -33,13 +33,39 @@ app.post("/books", (req,res)=>{
         req.body.desc,
         req.body.cover,
         req.body.price,
-    ]
+    ];
     
     db.query(q,[values], (err,data)=>{
         if(err)return res.json(err)
         return res.json("Book has been created")
-    })
-})
+    });
+});
+
+app.delete("/books/:id", (req,res)=>{
+    const bookId = req.params.id;
+    const q = "DELETE FROM books WHERE id = ?"
+
+    db.query(q, [bookId], (err,data)=>{
+        if(err)return res.json(err)
+        return res.json("Book has been deleted")
+    });
+});
+
+app.put("/books/:id", (req,res)=>{
+    const bookId = req.params.id;
+    const q = "UPDATE books SET `title` = ?, `desc` = ?, `cover` = ?, `price` = ? WHERE id = ?";
+    const values = [
+        req.body.title,
+        req.body.desc,
+        req.body.cover,
+        req.body.price,
+    ];
+
+    db.query(q, [...values, bookId], (err,data)=>{
+        if(err)return res.json(err)
+        return res.json("Book has been updated")
+    });
+});
 
 app.listen(8800, ()=>{
     console.log("connected to backend!")
